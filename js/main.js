d08005c563d7d36b5a1fdd8ff79412a8151263e8
System Administrator <root@Camilo-wifi.local>
2019-09-05 17:55:10 -0500

    primer landing

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('.carou-beneficios').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:true,
+	    autoplay:true,
+	    autoplayTimeout:10000,
+	    autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+	})
+	$('.carou-proyectos').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    nav:true,
+	    items:1
+	})
+});
