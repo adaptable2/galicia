fec2f19b412dc230906177963fccc8ab494d04ea
System Administrator <root@Camilo-wifi.local>
2019-09-09 18:10:04 -0500

    final landing

--- js/main.js
+++ js/main.js
@@ -24,4 +24,40 @@ $(document).ready(function(){
 	    nav:true,
 	    items:1
 	});
+
+$(function(){
+	$(".machine").typed({
+		strings: ["el confort.", "el estatus.", "la innovación."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
 });
